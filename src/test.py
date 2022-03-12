54c3dedc0f44a92482f2965f923b331285f01071
manasparanjape <mparanja@purdue.edu>
2022-03-12 11:31:19 -0500

    Saves weights and biases to file

--- src/test.py
+++ src/test.py
@@ -1,46 +1,21 @@
 import numpy as np
-import cv2
-from imutils.contours import sort_contours
-from matplotlib import pyplot as plt
-import imutils
 from PIL import Image as img
+from PIL import ImageOps as imgops
+import math
 
-gray = cv2.imread("C:/NN/NIST SD 19/Words/Test 0.jpg", 0)
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(blurred, 100, 200)
-# cv2.imshow("edged", edged)
-# cv2.waitKey(0)
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sort_contours(cnts, method="left-to-right")[0]
+pixels = 50
 
+test_image = img.open("C:/NN/NIST SD 19/Words/Test 0.jpg")
+test_image = imgops.grayscale(test_image)
+test_image = np.asarray(test_image)
 
-chars = []
+print(np.shape(test_image))
 
-for c in cnts:
-    (x, y, w, h) = cv2.boundingRect(c)
-    if (w >= 5 and w <= 150) and (h >= 15 and h <= 120):
-        roi = gray[y:y + h, x:x + w]
-        thresh = cv2.threshold(roi, 0, 255,
-                               cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        (tH, tW) = thresh.shape
-        if tW > tH:
-            thresh = imutils.resize(thresh, width=32)
-        else:
-            thresh = imutils.resize(thresh, height=32)
+test_image = np.asarray((img.fromarray(test_image)).resize((math.ceil(pixels * (np.shape(test_image)[1] / np.shape(test_image)[0])), pixels)))
+print(np.shape(test_image))
+print(np.shape(test_image[0:40][0:50]))
 
-        (tH, tW) = thresh.shape
-        dX = int(max(0, 32 - tW) / 2.0)
-        dY = int(max(0, 32 - tH) / 2.0)
-        padded = cv2.copyMakeBorder(thresh, top=dY, bottom=dY,
-                                    left=dX, right=dX, borderType=cv2.BORDER_CONSTANT,
-                                    value=(0, 0, 0))
-        padded = cv2.resize(padded, (50, 50))
-        padded = padded.astype("float32") / 255.0
-        padded = np.expand_dims(padded, axis=-1)
-        padded = cv2.resize(padded, (50, 50))
-        cv2.imshow("image", padded)
-        cv2.waitKey(0)
-        print(np.shape(np.asarray(padded)))
-        chars.append((padded, (x, y, w, h)))
+for i in range(0, math.ceil(pixels * (np.shape(test_image)[1] / np.shape(test_image)[0])) - pixels, 5):
+    print(i)
+    # img.fromarray(test_image[:][i:(i + pixels)]).show()
+    print(np.shape(test_image[:][i:(i + pixels)]))
