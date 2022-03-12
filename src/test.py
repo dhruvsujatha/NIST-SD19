5b23a0ef8aa9c864d06f2f225700a561a51d4667
manasparanjape <mparanja@purdue.edu>
2022-03-12 12:28:05 -0500

    Saves weights and biases to file

--- src/test.py
+++ src/test.py
@@ -13,9 +13,9 @@ print(np.shape(test_image))
 
 test_image = np.asarray((img.fromarray(test_image)).resize((math.ceil(pixels * (np.shape(test_image)[1] / np.shape(test_image)[0])), pixels)))
 print(np.shape(test_image))
-print(np.shape(test_image[0:40][0:50]))
+print(np.shape(test_image[:, 0:30]))
 
-for i in range(0, math.ceil(pixels * (np.shape(test_image)[1] / np.shape(test_image)[0])) - pixels, 5):
+for i in range(0, math.ceil(pixels * (np.shape(test_image)[1] / np.shape(test_image)[0])) - pixels, 3):
     print(i)
-    # img.fromarray(test_image[:][i:(i + pixels)]).show()
-    print(np.shape(test_image[:][i:(i + pixels)]))
+    img.fromarray(test_image[:, i:(i + pixels)]).show()
+    print(np.shape(test_image[:, i:(i + pixels)]))
